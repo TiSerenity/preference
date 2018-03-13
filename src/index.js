72954d0c15ba1225f66e3a03cee83d61a2acc5b3
TiSerenity <fyfengyu@hotmail.com>
2018-03-13 22:55:52 +0800

    fff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
+const App=(props,context)=>{
+  return (
+    <div>
+      <Greeting who={'world'} />
+      <Greeting who={'react'} />
+    </div>
+  )
+}
 class Greeting extends Component {
+  render() {
+    return (
+      <div>  
+          <h1>{`hello ${this.props.who}`}</h1> 
+      </div>
+    );
+  }
+  //点击按钮转换文字
   state={
     who:'world'
   }
   switchName=()=>{
-
     if (this.state.who==='world')
       this.setState((prevState,props)=>({who:'react'}))
     else
@@ -20,3 +35,6 @@ class Greeting extends Component {
     );
   }
 }
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
