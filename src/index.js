e27c5974c650243579b8071c029ae47609d978b9
TiSerenity <fyfengyu@hotmail.com>
2018-03-13 22:31:25 +0800

    fff

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
-const App=(props,context)=>{
-  return (
-    <div>
-      <Greeting who={'world'} />
-      <Greeting who={'react'} />
-    </div>
-  )
-}
 class Greeting extends Component {
+  state={
+    who:'world'
+  }
+  switchName=()=>{
+
+    if (this.state.who==='world')
+      this.setState((prevState,props)=>({who:'react'}))
+    else
+      this.setState((prevState,props)=>({who:'world'}))
+  }
   render() {
     return (
       <div>  
-          <h1>{`hello ${this.props.who}`}</h1> 
+          <h1>{`hello ${this.state.who}`}</h1> 
+          <button onClick={this.switchName}> {this.state.who==='world'?'react':'world'}</button>
       </div>
     );
   }
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
