8f3b68de2f888bec41f1b88894c163eef407e6c4
TiSerenity <fyfengyu@hotmail.com>
2018-03-19 22:20:12 +0800

    fff

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Greeting extends Component {
   state={
     who:this.props.defaultWho
   }
-  }
+  
   switchName=()=>{
     if (this.state.who==='world')
       this.setState((prevState,props)=>({who:'react'}))
@@ -41,6 +41,45 @@ class Greeting extends Component {
     );
   }
 }
+  class GreetingInput extends Component {
+
+    constructor(props,context) {
+      super(props,context); //执行基类的构造函数
+  
+      this.state = {
+        who:this.props.defaultWho?this.props.defaultWho:''
+      };
+  
+      this.handleChange = this.handleChange.bind(this);
+      
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+  
+      // 如果构造函数没有绑定此函数，则在浏览器中按F12，查看condole,这里输出的this是什么？
+      console.log('this指针是:',this)
+      this.setState({who: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('如果您按回车，或者点击提交按钮，则窗体会执行提交事件，现在状态中who的值是: ' + this.state.who);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+        <h1>{`hello ${this.state.who}`}</h1> 
+        <label>
+          Who:
+          <input type="text" value={this.state.who} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="窗体提交按钮，可点击也可按回车，会触发form的onSubmit事件" />
+      </form>
+      );
+    }
+  }
 
 
 
